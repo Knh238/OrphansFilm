44d802880b1ee6b56a494e2fb3c993ce7fcd8081
Kristin <kristinnharper@gmail.com>
2018-11-29 14:08:49 -0500

    saved

--- my-app/src/components/Conference.js
+++ my-app/src/components/Conference.js
@@ -14,6 +14,7 @@ import { Link } from "react-router-dom";
 import { colors } from "@material-ui/core";
 import axios from "axios";
 import moment from "moment";
+
 const styles = {
   card: {
     maxWidth: 345,
